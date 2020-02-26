4465efdb02f36d948611595784fa6532e80c000a
Bsaunders1370 <50217809+Bsaunders1370@users.noreply.github.com>
2020-02-26 15:25:17 -0700

    Step 3 and 4

--- src/app.js
+++ src/app.js
@@ -4,3 +4,8 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
