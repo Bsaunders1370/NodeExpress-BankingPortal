548f613d72ad70de7133460bf5a569de13abd2be
Bsaunders1370 <50217809+Bsaunders1370@users.noreply.github.com>
2020-03-02 13:40:11 -0700

    Part 3 step 1

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+const accuntData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), utf8
+);
+const accounts = JSON.parse(accountData);
+
 app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
