f6057e5f0125a26534a7b452066e4116afc750bf
Bsaunders1370 <50217809+Bsaunders1370@users.noreply.github.com>
2020-03-03 16:22:42 -0700

    10

--- src/app.js
+++ src/app.js
@@ -41,11 +41,22 @@ accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseI
 const accountsJSON = JSON.stringify(accounts, null, 4);
 fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 res.render('transfer', {message: "Transfer Completed"});
-
 });
 
 app.get('/transfer', (req, res) => {
     res.render('profile', { user: users[0] });
+});
+
+app.get('/payment', (req, res) => {
+res.render('payment', { account: accounts.credit});
+});
+
+app.post('/payment', (req. res) => {
+accounts.credit.balance -= req.body.amount;
+accounts.credit.available += parseInt(req.body.amount, 10);
+const accountsJSON = JSON.stringify(accounts, null, 4);
+fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+res.render('payment', { message: "Payment Successful", account: accounts.credit})
 });
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
