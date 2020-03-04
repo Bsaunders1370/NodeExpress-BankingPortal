b8071cfcd76ddf5be49c694f68c08699a1a0af0d
Bsaunders1370 <50217809+Bsaunders1370@users.noreply.github.com>
2020-03-04 16:36:19 -0700

    13

--- src/app.js
+++ src/app.js
@@ -4,27 +4,22 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
-const { accounts, users, writeJSON } = require('./data');
+const { accounts, users, writeJSON } = require('./data.js');
+
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true })); 
-
-app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts}));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
 app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
-app.get('/profile', (req, res) => {
-    res.render('profile', { user: users[0] });
-});
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-
-const accountRoutes = require('./routes/accounts.js');  
-
-const servicesRoutes = require('./routes/services.js');
-
-app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
