86361d0cbfe66be2b8377ccd1b4b474a2bf5293b
Bsaunders1370 <50217809+Bsaunders1370@users.noreply.github.com>
2020-03-04 14:51:36 -0700

    10

--- src/app.js
+++ src/app.js
@@ -34,8 +34,8 @@ app.get('/profile', (req, res) => {
 app.post('/transfer', (req, res) => {
 accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
 accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-const accountsJSON = JSON.stringify(accounts, null, 4);
-fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+writeJSON();
 res.render('transfer', {message: "Transfer Completed"});
 });
 
@@ -49,8 +49,8 @@ res.render('payment', { account: accounts.credit});
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    
+   writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
